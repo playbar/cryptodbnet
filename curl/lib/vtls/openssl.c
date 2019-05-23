b87445dc2eb172852fe239e1c2ad11224c6e89f2
hougl <hgl868@126.com>
2019-05-23 20:50:52 +0800

    modify code

--- curl/lib/vtls/openssl.c
+++ curl/lib/vtls/openssl.c
@@ -2576,7 +2576,7 @@ static CURLcode ossl_connect_step1(struct connectdata *conn, int sockindex)
   /* OpenSSL 1.1.1 requires clients to opt-in for PHA */
   //todo
 //  SSL_CTX_set_post_handshake_auth(BACKEND->ctx, 1);
-  BACKEND->ctx->pha_enabled = 1;
+//  BACKEND->ctx->pha_enabled = 1;
 #endif
 
 #ifdef USE_TLS_SRP
